2830b4aab81f1eb3fcf2853ed57118f041fb55f0
Jake Mackinlay <jrmackinlay90@gmail.com>
2020-11-15 19:46:24 +1030

    Working application

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const inquirer = require("inquirer");
 const express = require("express");
 const table = require("console.table");
-const path = require("path");
 const mysql = require("mysql");
-let colors = require("colors");
 
 const app = express();
 
@@ -38,7 +36,6 @@ async function runPrompt() {
                     "Add Employees",
                     "Remove Employee",
                     "Update Role",
-                    "Update Manager",
                     "View All Roles",
                     "View All Departments",
                     "View All Managers",
@@ -52,7 +49,6 @@ async function runPrompt() {
             if (data.menu === "Add Employees") return addEmployee();
             if (data.menu === "Remove Employee") return removeEmployee();
             if (data.menu === "Update Role") return updateRole();
-            if (data.menu === "Update Manager") return updateManager();
             if (data.menu === "View All Roles") return viewAllRoles();
             if (data.menu === "View All Departments") return viewAllDepartments();
             if (data.menu === "View All Managers") return viewAllManagers();
@@ -96,6 +92,7 @@ async function runPrompt() {
                 message: "Select Department",
                 name: "department",
                 choices: [
+                    "Management",
                     "Finance",
                     "Legal",
                     "HR",
@@ -104,10 +101,20 @@ async function runPrompt() {
                 ]
             }
         ]).then(function(data) {
+            // MNAGEMENT
+            if (data.department === "Management") 
+                return connection.query(
+                    "SELECT DISTINCT employee.first_name, employee.last_name FROM employee, employee_role WHERE employee.manager_id = employee_role.department_id AND employee_role.department_id = 1", 
+                    function(err, res) {
+                        if (err) throw err;
+                        
+                        console.table(res);
+                        whatNext();
+                    });
             // FINANCE DEPT
             if (data.department === "Finance") 
                 return connection.query(
-                    "SELECT employee.first_name, employee.last_name, department.department_name FROM employee, department WHERE department.id = employee.role_id AND department.department_name = 'Finance'", 
+                    "SELECT DISTINCT employee.first_name, employee.last_name FROM employee, employee_role WHERE employee.manager_id = employee_role.department_id AND employee_role.department_id = 2", 
                     function(err, res) {
                         if (err) throw err;
                         
@@ -117,7 +124,7 @@ async function runPrompt() {
             // LEGAL DEPT
             if (data.department === "Legal")
                 return connection.query(
-                    "SELECT employee.first_name, employee.last_name, department.department_name FROM employee, department WHERE department.id = employee.role_id AND department.department_name = 'Legal'", 
+                    "SELECT DISTINCT employee.first_name, employee.last_name FROM employee, employee_role WHERE employee.manager_id = employee_role.department_id AND employee_role.department_id = 3", 
                     function(err, res) {
                         if (err) throw err;
 
@@ -127,7 +134,7 @@ async function runPrompt() {
             // HR DEPT
             if (data.department === "HR")
                 return connection.query(
-                    "SELECT employee.first_name, employee.last_name, department.department_name FROM employee, department WHERE department.id = employee.role_id AND department.department_name = 'HR'", 
+                    "SELECT DISTINCT employee.first_name, employee.last_name FROM employee, employee_role WHERE employee.manager_id = employee_role.department_id AND employee_role.department_id = 4", 
                     function(err, res) {
                         if (err) throw err;
 
@@ -137,7 +144,7 @@ async function runPrompt() {
             // SALES DEPT
             if (data.department === "Sales")
                 return connection.query(
-                    "SELECT employee.first_name, employee.last_name, department.department_name FROM employee, department WHERE department.id = employee.role_id AND department.department_name = 'Sales'", 
+                    "SELECT DISTINCT employee.first_name, employee.last_name FROM employee, employee_role WHERE employee.manager_id = employee_role.department_id AND employee_role.department_id = 5", 
                     function(err, res) {
                         if (err) throw err;
 
@@ -169,7 +176,7 @@ async function runPrompt() {
             }
         ]).then(function(data) {
             if (data.managers === "Finance Manager")
-                return connection.query("SELECT employee.first_name, employee.last_name, department.department_name, employee.manager_id FROM employee, department, employee_role WHERE employee.manager_id = employee_role.id = department.id AND employee_role.id = 1",
+                return connection.query("SELECT DISTINCT employee.first_name, employee.last_name, employee.manager_id FROM employee, employee_role WHERE employee.manager_id = 2",
                 function(err, res) {
                     if (err) throw err;
 
@@ -177,7 +184,7 @@ async function runPrompt() {
                     back();
                 });
             if (data.managers === "Legal Manager")
-                return connection.query("SELECT employee.first_name, employee.last_name, department.department_name, employee.manager_id FROM employee, department, employee_role WHERE employee.manager_id = employee_role.id = department.id AND employee_role.id = 2",
+                return connection.query("SELECT DISTINCT employee.first_name, employee.last_name, employee.manager_id FROM employee, employee_role WHERE employee.manager_id = 3",
                 function(err, res) {
                     if (err) throw err;
 
@@ -185,7 +192,7 @@ async function runPrompt() {
                     back();
                 });
             if (data.managers === "HR Manager")
-                return connection.query("SELECT employee.first_name, employee.last_name, department.department_name, employee.manager_id FROM employee, department, employee_role WHERE employee.manager_id = employee_role.id = department.id AND employee_role.id = 3",
+                return connection.query("SELECT DISTINCT employee.first_name, employee.last_name, employee.manager_id FROM employee, employee_role WHERE employee.manager_id = 4",
                 function(err, res) {
                     if (err) throw err;
 
@@ -193,7 +200,7 @@ async function runPrompt() {
                     back();
                 });
             if (data.managers === "Sales Manager")
-                return connection.query("SELECT employee.first_name, employee.last_name, department.department_name, employee.manager_id FROM employee, department, employee_role WHERE employee.manager_id = employee_role.id = department.id AND employee_role.id = 4",
+                return connection.query("SELECT DISTINCT employee.first_name, employee.last_name, employee.manager_id FROM employee, employee_role WHERE employee.manager_id = 5",
                 function(err, res) {
                     if (err) throw err;
 
@@ -268,7 +275,7 @@ async function runPrompt() {
                     type: "list",
                     message: "Select Employee",
                     name: "removal",
-                    loop: false,
+                    // loop: false,
                     choices: 
                         function() {
                             let employeeArray = [];
@@ -277,26 +284,16 @@ async function runPrompt() {
                             }
                             return employeeArray;
                         },
-                }
-
-                
+                }                
             ]).then(function(data) {
-                console.log(data);
-
                 let deleteEmployee = JSON.stringify(data.removal);
 
-                console.log(deleteEmployee);
-
                 let fired = deleteEmployee.slice(5, 8);
-
-                console.log(fired);
 
                 if (data.removal === data.removal) {
                     connection.query("DELETE FROM employee WHERE id = " + fired, function(err, res) {
                         if (err) throw err;
                 
-        
-                        console.log(res);
                         console.log(`${data.removal} HAS BEEN REMOVED`);
 
                         inquirer.prompt([
@@ -325,25 +322,171 @@ async function runPrompt() {
     // Update employee role (ie promotion/demotion etc)
     function updateRole() {
         console.log("\nUpdating Role\n");
-    };
 
-    // Update manager (assign new employee to manager role)
-    function updateManager() {
-        console.log("\nUpdating Managers\n");
+        connection.query("SELECT employee.id, employee.first_name, employee.last_name, employee_role.title FROM employee INNER JOIN employee_role ON employee.role_id = employee_role.id", function(err, res) {
+            if (err) throw err;
+
+            inquirer.prompt([
+                {
+                    type: "list",
+                    message: "What would you like to do?",
+                    name: "selection",
+                    choices: [
+                        "Assign Employee To New Department",
+                        "Go Back"
+                    ]
+                }
+            ]).then(function(data) {
+                if (data.selection === "Go Back") return menu();
+                if (data.selection === "Assign Employee To New Department") {
+                    inquirer.prompt([
+                        {
+                            type: "list",
+                            message: "Which Employee Is Moving?",
+                            name: "selection",
+                            choices: function() {
+                                let employeeArray = [];
+                                for (let i = 0; i < res.length; i++) {
+                                    employeeArray.push(`ID: ${res[i].id} ---- Role ID: ${res[i].role_id} ---- Name: ${res[i].first_name} ${res[i].last_name}`);
+                                }
+                                return employeeArray;
+                            }
+                        }
+                    ]).then(function(data) {
+                        if (data.selection === data.selection) {
+                            let movingEmployee = JSON.stringify(data.selection);
+                            let movingStr = movingEmployee.slice(4,7);
+                            console.log(movingStr);
+                            connection.query(`SELECT title, id FROM employee_role`, function(err, res) {
+                                if (err) throw err;
+
+                                inquirer.prompt([
+                                    {
+                                        type: "list",
+                                        message: "Which Department Are They Moving Too",
+                                        name: "selection",
+                                        choices: function() {
+                                            let employeeArray = [];
+                                            for (let i = 0; i < res.length; i++) {
+                                                employeeArray.push(`ID: ${res[i].id} --- DEPARTMENT: ${res[i].title}`);
+                                            }
+                                            return employeeArray;
+                                        }
+                                    }
+                                ]).then(function(data) {
+                                    console.log(data);
+                                    if (data.selection === data.selection) {
+
+                                        let employeeMoving = JSON.stringify(data.selection);
+                                        let employeeStr = employeeMoving.slice(4,7);
+
+                                        console.log(employeeStr);
+                                        connection.query(`UPDATE employee SET role_id = ${employeeStr} WHERE id = ${movingStr}`,function(err, res) {
+                                            if (err) throw err;
+
+                                            console.log("SUCCESS")
+                                            inquirer.prompt([
+                                                {
+                                                    type: "list",
+                                                    message: "What Next?",
+                                                    name: "next",
+                                                    choices: [
+                                                        "Go Back",
+                                                        "View Employees",
+                                                        "Update Another Employee"
+                                                    ]
+                                                }
+                                            ]).then(function(data) {
+                                                if (data.next === "Go Back") return menu();
+                                                if (data.next === "View Employees") return viewAllEmployees();
+                                                if (data.next === "Update Another Employee") return updateRole();
+                                            })
+                                        })
+                                    }
+                                })
+                            })
+                        }
+                    })
+                }
+            })
+        })
     };
 
     function viewAllRoles() {
         console.log("\nSee all the Roles\n");
+
+        connection.query("SELECT * FROM employee_role", function(err, res) {
+            if (err) throw err;
+
+            console.table(res);
+            inquirer.prompt([
+                {
+                    type: "list",
+                    message: "What Next?",
+                    name: "next",
+                    choices: [
+                        "Go Back",
+                        "Add Employee",
+                        "Remove Employee"
+                    ]
+                }
+            ]).then(function(data) {
+                if (data.next === "Go Back") return menu();
+                if (data.next === "Add Employee") return addEmployee();
+                if (data.next === "Remove Employee") return removeEmployee();
+            })
+        })
     };
 
     // Search through departments and make changes from there
     function viewAllDepartments() {
         console.log("\nSee all the Departments\n");
+
+        connection.query("SELECT * FROM department", function(err, res) {
+            if (err) throw err;
+
+            console.table(res);
+            inquirer.prompt([
+                {
+                    type: "list",
+                    message: "What Next?",
+                    name: "next",
+                    choices: [
+                        "Go Back",
+                        "Add Employee",
+                        "Remove Employee"
+                    ]
+                }
+            ]).then(function(data) {
+                if (data.next === "Go Back") return menu();
+                if (data.next === "Add Employee") return addEmployee();
+                if (data.next === "Remove Employee") return removeEmployee();
+            })
+        })
     };
 
     // Show all managers and view/update info from there
     function viewAllManagers() {
         console.log("\nSee all the Managers\n");
+
+        connection.query("SELECT DISTINCT employee_role.title, employee.first_name, employee.last_name FROM employee, employee_role WHERE employee_role.id = employee.role_id AND employee_role.department_id = 1", 
+        function(err, res) {
+            if (err) throw err;
+
+            console.table(res);
+            inquirer.prompt([
+                {
+                    type: "list",
+                    message: "What Next?",
+                    name: "next",
+                    choices: [
+                        "Go Back?",
+                    ]
+                }
+            ]).then(function(data) {
+                if (data.next === "Go Back?") return menu();
+            })
+        })
     };
 
     // Leave the application
@@ -352,6 +495,7 @@ async function runPrompt() {
         connection.end();
     };
     
+    // Global "WHAT NEXT" function
     async function whatNext() {
         inquirer.prompt([
             {
@@ -361,16 +505,17 @@ async function runPrompt() {
                 choices: [
                     "Add Employee to Department",
                     "Remove Employee from Department",
-                    "Exit"
+                    "Go Back"
                 ]
             }
         ]).then(function(data) {
             if (data.next === "Add Employee to Department") return addEmployee();
             if (data.next === "Remove Employee from Department") return removeEmployee();
-            if (data.next === "Exit") return menu();
+            if (data.next === "Go Back") return menu();
         });
     };
 
+    // Global "GO BACK" function
     async function back() {
         inquirer.prompt([
             {
